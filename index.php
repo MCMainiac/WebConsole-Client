afbb708ba76c38aca41dc030aaf57fe40633ef72
Ricardo <ricardo.boss@web.de>
2016-12-05 00:37:28 +0100

    some changes for a fancier interface :)

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
-set_time_limit(30);
+require __DIR__."/methods/autoload.php";
 
-require_once __DIR__."/autoload.php";
-
-use WebConsole\Address;
+use WebConsole\utils\Address;
 use WebConsole\Channel;
-use WebConsole\ClientCommand;
+use WebConsole\commands\ClientCommand;
 
 $address = new Address("localhost", 1424);
 $client = new Channel($address);
-$test = "Dies ist ein Test!";
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,20 +15,54 @@ $test = "Dies ist ein Test!";
 </head>
 <body>
 <h1>WebConsole <small>by MCMainiac</small></h1>
-	<pre>
-		<?php
-			if(isset($_POST['command'])) {
-				$client->connect();
+	<pre><?php
 
-				#$client->ping(array($test));
-				$client->sendCommand(new ClientCommand(ClientCommand::MC_COMMAND), array($_POST['command']));
-				#$client->sendCommand(new ClientCommand(ClientCommand::MC_COMMAND), array("stop"));
+        echo PHP_EOL;
 
-				$client->disconnect();
-				echo "<div id=\"success\">Command successfully executed on client!</div>";
-			}
-		?>
-	</pre>
+        try {
+	        $client->connect();
+
+	        echo "Connected to server!" . PHP_EOL;
+        } catch (Exception $e) {
+            echo "There was an error connecting to the server (perhaps the server isn't running?):".PHP_EOL;
+            echo $e->getMessage().PHP_EOL;
+            echo "[Debug]: Thrown at " . $e->getFile() . ":" . $e->getLine();
+
+            goto end;
+        }
+
+        if(isset($_POST['command'])) {
+            $packet = $client->sendCommand(
+                    new ClientCommand(ClientCommand::MC_COMMAND),
+                array($_POST['command'])
+            );
+
+            if ($packet === false)
+                $success = false;
+            else
+                $success = true;
+
+            if ($success) {
+                echo "<strong><u>Response:</u></strong>" . PHP_EOL;
+                echo "<strong>Packet id:</strong> " . $packet->getId() . PHP_EOL;
+                echo "<strong>Response string:</strong> " . $packet->getCommandString() . PHP_EOL;
+                echo "<strong>Arguments (concatenated):</strong>" . PHP_EOL;
+                echo implode(
+                        \WebConsole\packet\Packet::ARGUMENTS_DELIMITER,
+                    $packet->getArguments()
+                );
+            } else {
+                echo "<strong>Failed to send packet!</strong>" . PHP_EOL;
+            }
+        }
+
+        $client->disconnect();
+
+        end:
+
+        echo PHP_EOL;
+    ?></pre>
+    <br>
 	<form action="./" method="post">
 		Command: <input type="text" name="command" placeholder="enter a command to execute" /><br>
 		<input type="submit" />
